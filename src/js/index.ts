ac205ac48ef2f2cc2507aca7218e3419c02a8647
utkarsh4321 <utkarshsrivastava434@gmail.com>
2020-07-11 20:49:09 +0530

    workflow completed with webpack

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,10 +1,6 @@
-// Function on the typescript
-// import { intro } from "./checj";
-// console.log("ram is here");
-// console.log("How are you");
-// console.log(intro("ut"));
-// import "../sass/style.scss";
+import "../sass/style.scss";
 
 import { intro } from "./checj";
 console.log("are you now ready");
 console.log(intro("ram"));
+alert("hhi");
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/js/index.ts",
@@ -7,15 +9,17 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.html$/,
-        use: ["html-loader"],
-      },
-      {
         test: /\.scss$/,
         exclude: /node_modules/,
+        // loader: "css-loader",
+        // options: {
+        //   modules: true,
+        // },
+
         use: [
-          "style-loader",
+          MiniCssPlugin.loader,
           "css-loader",
+
           {
             loader: "sass-loader",
             options: {
@@ -29,27 +33,17 @@ module.exports = {
         ],
       },
       {
-        test: /\.tsx?$/,
+        test: /\.ts?$/,
         use: "ts-loader",
         include: [path.resolve((__dirname, "src"))],
       },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-      {
-        test: /\.(svg|png|jpg|gif)$/,
-        use: {
-          loader: "file-loader",
-          options: {
-            name: "[name].[hash].[ext]",
-            outputPath: "./public/img",
-          },
-        },
-      },
+      // {
+      //   test: /\.js$/,
+      //   exclude: /node_modules/,
+      //   use: {
+      //     loader: "babel-loader",
+      //   },
+      // },
     ],
   },
   resolve: {
@@ -63,6 +57,10 @@ module.exports = {
       filename: "index.html",
       template: "./src/index.html",
     }),
+    new MiniCssPlugin({
+      filename: "[name].[hash].css",
+    }),
+    new CleanWebpackPlugin(),
   ],
   output: {
     // publicPath: "public",
